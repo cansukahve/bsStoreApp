4debe25f198ae3c6b1698e4829909811932fa05d
cansukahve <52cansukahve@gmail.com>
2025-02-10 15:03:27 +0300

    Hateoas are complete.

--- Services/BookLinks.cs
+++ Services/BookLinks.cs
@@ -3,15 +3,11 @@ using Entities.LinkModels;
 using Entities.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Routing;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Net.Http.Headers;
 using Services.Contract;
 using Services.Contracts;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Reflection.Metadata.Ecma335;
-using System.Text;
-using System.Threading.Tasks;
+using System.ComponentModel.Design;
 
 namespace Services
 {
@@ -26,16 +22,15 @@ namespace Services
             _linkGenerator = linkGenerator;
             _dataShaper = dataShaper;
         }
+
         public LinkResponse TryGenerateLinks(IEnumerable<BookDto> booksDto,
             string fields,
             HttpContext httpContext)
         {
-            var shapedBooks = ShepeData(booksDto, fields);
+            var shapedBooks = ShapeData(booksDto, fields);
             if (ShouldGenerateLinks(httpContext))
                 return ReturnLinkedBooks(booksDto, fields, httpContext, shapedBooks);
             return ReturnShapedBooks(shapedBooks);
-
-
         }
 
         private LinkResponse ReturnLinkedBooks(IEnumerable<BookDto> booksDto,
@@ -45,33 +40,55 @@ namespace Services
         {
             var bookDtoList = booksDto.ToList();
 
-            for (var index = 0; index < bookDtoList.Count(); index++)
+            for (int index = 0; index < bookDtoList.Count(); index++)
             {
                 var bookLinks = CreateForBook(httpContext, bookDtoList[index], fields);
                 shapedBooks[index].Add("Links", bookLinks);
             }
 
             var bookCollection = new LinkCollectionWrapper<Entity>(shapedBooks);
+            CreateForBooks(httpContext, bookCollection);
             return new LinkResponse { HasLinks = true, LinkedEntities = bookCollection };
-
         }
 
-        private List<Link> CreateForBook(HttpContext httpContext, BookDto bookDto, string fields)
+        private LinkCollectionWrapper<Entity> CreateForBooks(HttpContext httpContext,
+            LinkCollectionWrapper<Entity> bookCollectionWrapper)
+        {
+            bookCollectionWrapper.Links.Add(new Link()
+            {
+                Href = $"/api/{httpContext.GetRouteData().Values["controller"].ToString().ToLower()}",
+                Rel = "self",
+                Method = "GET"
+            });
+            return bookCollectionWrapper;
+        }
+
+        private List<Link> CreateForBook(HttpContext httpContext,
+            BookDto bookDto,
+            string fields)
         {
             var links = new List<Link>()
             {
-                new Link("a1", "b1" ,"c1"),
-                new Link("a2", "b2" ,"c2")
-             };
+               new Link()
+               {
+                   Href = $"/api/{httpContext.GetRouteData().Values["controller"].ToString().ToLower()}" +
+                   $"/{bookDto.Id}",
+                   Rel = "self",
+                   Method = "GET"
+               },
+               new Link()
+               {
+                   Href = $"/api/{httpContext.GetRouteData().Values["controller"].ToString().ToLower()}",
+                   Rel="create",
+                   Method = "POST"
+               },
+            };
             return links;
         }
 
         private LinkResponse ReturnShapedBooks(List<Entity> shapedBooks)
         {
-            return new LinkResponse()
-            {
-                ShapedEntities = shapedBooks
-            };
+            return new LinkResponse() { ShapedEntities = shapedBooks };
         }
 
         private bool ShouldGenerateLinks(HttpContext httpContext)
@@ -82,13 +99,14 @@ namespace Services
                 .EndsWith("hateoas", StringComparison.InvariantCultureIgnoreCase);
         }
 
-        private List<Entity> ShepeData(IEnumerable<BookDto> booksDto, string fields)
+        private List<Entity> ShapeData(IEnumerable<BookDto> booksDto, string fields)
         {
             return _dataShaper
                 .ShapeData(booksDto, fields)
                 .Select(b => b.Entity)
                 .ToList();
-
         }
+
+
     }
 }
