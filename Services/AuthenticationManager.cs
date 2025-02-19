6f02cf4099ca8fff463592e4810cf3a5c4b978c2
cansukahve <52cansukahve@gmail.com>
2025-02-19 12:42:55 +0300

    User verification step has been done.

--- Services/AuthenticationManager.cs
+++ Services/AuthenticationManager.cs
@@ -18,7 +18,7 @@ namespace Services
         private readonly UserManager<User> _userManager;
         private readonly IConfiguration _configuration;
 
-        //private User? _user;
+        private User? _user;
 
         public AuthenticationManager(ILoggerService logger,
             IMapper mapper,
@@ -43,6 +43,16 @@ namespace Services
             return result;
         }
 
+        public async Task<bool> ValidateUser(UserForAuthenticationDto userForAuthDto)
+        {
+            _user = await _userManager.FindByNameAsync(userForAuthDto.UserName);
+            var result = (_user != null && await _userManager.CheckPasswordAsync(_user, userForAuthDto.Password));
+            if (!result)
+            {
+                _logger.LogWarning($"{nameof(ValidateUser)}: Authentication failed. Wrong user name or password.");
+            }
+            return result;
+        }
     }
 }
 
--- Services/Contracts/IAuthenticationService.cs
+++ Services/Contracts/IAuthenticationService.cs
@@ -7,6 +7,7 @@ namespace Services.Contracts
     public interface IAuthenticationService
     {
         Task<IdentityResult> RegisterUser(UserForRegistrationDto userForRegistrationDto);
+        Task<bool> ValidateUser(UserForAuthenticationDto userForAuthDto);
     }
 
 }
